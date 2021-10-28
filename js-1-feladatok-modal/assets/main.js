f734de8be81c1a102e48e92f173b5da9b2e32e87
klvinbagoly <kbalazsk1@gmail.com>
2021-10-28 09:21:02 +0200

    Maybe ready...

---
+++ js-1-feladatok-modal/assets/main.js
@@ -0,0 +1,26 @@
+function openModal (){
+    const modal = document.querySelector('.modal');
+    modal.setAttribute('class','modal-visible');
+    document.body.setAttribute('style', 'background-color: rgba(11, 11, 11, 0.8);');
+    setTimeout(() =>
+    document.body.addEventListener('click', closeModal), 1000
+    )
+    modal.addEventListener('click', function(ev){
+        ev.stopPropagation()});
+}
+
+// function stopClosing (ev){
+//     ev.stopPropagation()
+// }
+
+// function setEventListener (){
+//     let background = document.querySelector('body:not".modal"')
+//     background.addEventListener('click', closeModal)
+// }
+
+function closeModal (){
+    const modal = document.querySelector('.modal-visible');
+    modal.setAttribute('class','modal');
+    document.body.setAttribute('style', 'background-color:  rgba(55, 55, 55, 0.2)');
+    document.body.removeEventListener('click', closeModal);
+}
