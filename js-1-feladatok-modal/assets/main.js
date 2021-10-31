8e3b862cf347d6a2869437b094b8247f9ab56039
klvinbagoly <kbalazsk1@gmail.com>
2021-10-31 16:57:34 +0100

    New version with overlay

--- js-1-feladatok-modal/assets/main.js
+++ js-1-feladatok-modal/assets/main.js
@@ -1,31 +1,38 @@
+// <ℹ️> A kommentelt részek csak a kód korábbi verzióját dokumentálják. </ℹ️>
 function openModal (){
     const modal = document.querySelector('.modal');
     modal.setAttribute('class','modal-visible');
-    document.body.setAttribute('style', 'background-color: rgba(11, 11, 11, 0.8);');
     // setTimeout(() =>
     // document.body.addEventListener('click', closeModal), 1000
     // )
     // modal.addEventListener('click', ev =>
     //     ev.stopPropagation()); HELYETT:
-    const container = document.querySelector('.container');
-    window.addEventListener('click', function(ev) {
-        if (ev.target == container ||ev.target == document.body ||ev.target == document.body.parentElement){
-            closeModal();
-        }})
+    //document.body.setAttribute('style', 'background-color: rgba(11, 11, 11, 0.8);');
+    // const container = document.querySelector('.container');
+    // window.addEventListener('click', function(ev) {
+    //     if (ev.target == container ||ev.target == document.body ||ev.target == document.body.parentElement){
+    //         closeModal();
+    //     }}); HELYETT:
+    const overlay = document.querySelector('.overlay');
+    overlay.classList.add('overlay-visible');
+    overlay.addEventListener('click', closeModal);
 }
 
 // function stopClosing (ev){
 //     ev.stopPropagation()
-// }
+// }  => Nem kell.
 
 // function setEventListener (){
-//     let background = document.querySelector('body:not".modal"')
+//     let background = document.querySelector('body:not".modal"') =>Nem működik XD
 //     background.addEventListener('click', closeModal)
-// }
+// }   
 
 function closeModal (){
     const modal = document.querySelector('.modal-visible');
     modal.setAttribute('class','modal');
-    document.body.setAttribute('style', 'background-color:  rgba(55, 55, 55, 0.2)');
-    document.body.removeEventListener('click', closeModal);
+    //document.body.setAttribute('style', 'background-color:  rgba(55, 55, 55, 0.2)');
+    // document.body.removeEventListener('click', closeModal); HELYETT:
+    const overlay = document.querySelector('.overlay');
+    overlay.classList.remove('overlay-visible');
+    overlay.removeEventListener('click', closeModal);
 }
