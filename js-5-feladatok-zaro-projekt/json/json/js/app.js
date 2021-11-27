85572de4565272419334fb0036f5d038dd87cfc4
klvinbagoly <kbalazsk1@gmail.com>
2021-11-27 23:58:55 +0100

    hampir siap

--- js-5-feladatok-zaro-projekt/json/json/js/app.js
+++ js-5-feladatok-zaro-projekt/json/json/js/app.js
@@ -1,6 +1,7 @@
 const userKeys = ['id', 'name', 'address', 'email']
+const nonWriteableUsers = []
 
-const getUsers = async (url) => {
+const getUsers = (url) => {
   const fetchOptions = {
     method: "GET",
     mode: "cors",
@@ -39,6 +40,11 @@ const fillDataTable = (data) => {
     }
     const btnGroup = tr.insertCell()
     btnGroup.appendChild(createBtnGroup())  
+    if (nonWriteableUsers.includes(row.id)){
+     btnGroup.querySelectorAll('button').forEach(btn => btn.onclick = () => {
+       console.log('Non-writeable user')
+     })
+    }
   }
   const thead = createAnyElement('thead');
   const the = thead.insertRow()
@@ -48,7 +54,6 @@ const fillDataTable = (data) => {
   }
    const last = the.insertCell()
    last.textContent = 'Actions'
-  console.log(thead)
    table.appendChild(thead)
 }
 
@@ -72,9 +77,9 @@ const createAnyElement = (elem, attributes = {}) => {
 
 const createBtnGroup = () => {
   const btnGroup = createAnyElement('div', {class: 'btn-group'});
-  const btnEdit = createAnyElement('button', {class: 'btn btn-edit', onclick: 'editUser(event, this)'});
+  const btnEdit = createAnyElement('button', {class: 'btn btn-edit', onclick: 'editUser(event, this)', type: 'button'});
   btnEdit.innerHTML = '<i class="fa fa-pencil-square-o" aria-hidden="true"></i>'  ;
-  const btnDel = createAnyElement('button', {class: 'btn btn-delete', onclick: 'deleteUser(this)'});
+  const btnDel = createAnyElement('button', {class: 'btn btn-delete', onclick: 'deleteUser(this)', type: 'button'});
   btnDel.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
 
   btnGroup.appendChild(btnEdit);
@@ -86,7 +91,6 @@ const createBtnGroup = () => {
 const deleteUser = (elem) => {
   let tr = elem.parentElement.parentElement.parentElement;
   let id = tr.firstElementChild.firstElementChild.value;
-  console.log(id)
   const fetchOptions = {
     method: 'DELETE',
     mode: 'cors',
@@ -101,7 +105,7 @@ const deleteUser = (elem) => {
   )}
 }
 
-const newUserRow = row => {
+const newUserRow = () => {
   let tr = createAnyElement('tr');
   for (let k of userKeys){
     let td = tr.insertCell()
@@ -109,6 +113,10 @@ const newUserRow = row => {
       class: 'new-user-input',
       name : k
     })
+    if (k === 'id'){
+      input.setAttribute('readonly', 'true');
+      input.value = 'Add user'
+    }
     td.appendChild(input)
   }
 
@@ -118,15 +126,18 @@ const newUserRow = row => {
   })
   newBtn.innerHTML = '<i class="fa fa-plus-circle" aria-hidden="true"></i>';
 
-  let td = createAnyElement('td');
+  let td = tr.insertCell();
   td.appendChild(newBtn)
-  tr.appendChild(td);
   return tr
 }
 
 const addUser = ( btn) => {
   
   const tr = btn.parentElement.parentElement;
+  if (!validateUser(tr)) {
+    showMessage('Invalid input!', 'danger', tr)
+    return
+  }
   const data = getRowData(tr);
   delete data.id;
   const fetchOptions = {
@@ -142,7 +153,7 @@ const addUser = ( btn) => {
     response => response.json(),
     err => console.error(err)
   ).then(
-    data => getUsers('http://localhost:3000/users')
+    () => getUsers('http://localhost:3000/users')
   )
 }
 
@@ -156,37 +167,71 @@ return data;
 }
 
 const editUser = (event,btn) => {
-  console.log(event)
   event.preventDefault();
   btn.innerHTML = '<i class="fa fa-floppy-o" aria-hidden="true"></i>';
 const btnUndo = btn.parentElement.lastElementChild
 btnUndo.innerHTML = '<i class="fa fa-undo" aria-hidden="true"></i>';
 
-
-  const tr = btn.parentElement.parentElement.parentElement;
-  const inputs = tr.querySelectorAll('input');
-  console.log(inputs)
-  inputs.forEach(input => {
-    if (input.name !== 'id'){
-      input.setAttribute('readonly', 'false')
-    }
-  })
-  
-  const data = getRowData(tr);
-   
-  const fetchOptions = {
-    method: "PUT",
-    mode: "cors",
-    cache: "no-cache",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
+const tr = btn.parentElement.parentElement.parentElement;
+const inputs = tr.querySelectorAll('input');
+console.log(inputs)
+inputs.forEach(input => {
+  if (input.name !== 'id'){
+    input.removeAttribute('readonly')
   }
-  fetch(`http://localhost:3000/users/${data.id}`, fetchOptions).then(
-    resp => resp.json(),
-    err => console.error(err)
-  ).then(
-    data => getUsers('http://localhost:3000/users')
-  )
+})
+btn.onclick = () => {if (validateUser(tr)){
+  saveUser(tr)
+}}
+btnUndo.onclick = () => getUsers('http://localhost:3000/users')
+   document.querySelectorAll('button').forEach(button => {
+     if (!tr.contains(button)){
+       button.onclick = () => {
+         showMessage("You can't edit multiple users at once.", 'danger', tr)
+       }
+     }
+   })
+}
+
+const validateUser = tr => {
+  const data = getRowData(tr);
+  const patterns = {
+    id: /.*/,
+    name: /^[A-Z][a-z]* [A-Z][a-z]*$/,
+    address: /^\d* ([A-Z0-9][a-z0-9 ]*)+$/,
+    email:    /^[a-z0-9\-.]+@[a-z0-9\-.]+\.[a-z]{2,4}$/
+  }
+  if (Object.keys(data).some(key => data[key].match(patterns[key]) === null)){
+    showMessage('Invalid input!', 'danger', tr)
+    return false
+  } else return true
+}
+
+const saveUser = (tr) => {
+  const data = getRowData(tr);
+  showMessage('Changes saved.', 'success', tr)
+  nonWriteableUsers.push(data.id)
+const fetchOptions = {
+  method: "PUT",
+  mode: "cors",
+  cache: "no-cache",
+  headers: {
+    "Content-Type": "application/json"
+  },
+  body: JSON.stringify(data)
+}
+fetch(`http://localhost:3000/users/${data.id}`, fetchOptions).then(
+  resp => resp.json(),
+  err => console.error(err)
+).then(
+  () => getUsers('http://localhost:3000/users')
+)}
+
+const showMessage = (message, type, row) => {
+  const td = row.insertCell();
+  td.classList.add(type);
+  td.textContent = message;
+  setTimeout(() =>{
+    row.removeChild(td)
+  }, 5000)
 }
