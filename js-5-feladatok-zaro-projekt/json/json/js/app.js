cbeb86a7e16f0361221693fed476cbdbf1c9a376
klvinbagoly <kbalazsk1@gmail.com>
2021-11-28 10:33:11 +0100

    One module created with big difficulty :)

--- js-5-feladatok-zaro-projekt/json/json/js/app.js
+++ js-5-feladatok-zaro-projekt/json/json/js/app.js
@@ -1,3 +1,10 @@
+import {
+  newUserRow,
+  addUser,
+  showMessage,
+  editUser
+} from './module1.js'
+
 const userKeys = ['id', 'name', 'address', 'email']
 const nonWriteableUsers = []
 
@@ -107,135 +114,14 @@ const deleteUser = (elem) => {
   )}
 }
 
-const newUserRow = () => {
-  let tr = createAnyElement('tr');
-  for (let k of userKeys){
-    let td = tr.insertCell()
-    let input = createAnyElement('input', {
-      class: 'new-user-input',
-      name : k
-    })
-    if (k === 'id'){
-      input.setAttribute('readonly', 'true');
-      input.value = 'Add user'
-    }
-    td.appendChild(input)
-  }
 
-  let newBtn = createAnyElement('button', {
-    class: 'btn btn-new',
-    onclick: 'addUser(this)'
-  })
-  newBtn.innerHTML = '<i class="fa fa-plus-circle" aria-hidden="true"></i>';
+window.editUser = editUser;
+window.deleteUser = deleteUser;
+window.addUser = addUser;
 
-  let td = tr.insertCell();
-  td.appendChild(newBtn)
-  return tr
-}
-
-const addUser = ( btn) => {
-  
-  const tr = btn.parentElement.parentElement;
-  if (!validateUser(tr)) {
-    showMessage('Invalid input!', 'danger', tr)
-    return
-  }
-  const data = getRowData(tr);
-  delete data.id;
-  const fetchOptions = {
-    method: "POST",
-    mode: "cors",
-    cache: "no-cache",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(data)
-  }
-  fetch(`http://localhost:3000/users`, fetchOptions).then(
-    response => response.json(),
-    err => console.error(err)
-  ).then(
-    () => getUsers('http://localhost:3000/users')
-  )
-}
-
-const getRowData = tr => {
-  const inputs = tr.querySelectorAll('input');
-  let data = {};
-  inputs.forEach(input => {
-    data[input.name] = input.value;
-  })
-return data;
-}
-
-const editUser = (event,btn) => {
-  btn.innerHTML = '<i class="fa fa-floppy-o" aria-hidden="true"></i>';
-const btnUndo = btn.parentElement.lastElementChild
-btnUndo.innerHTML = '<i class="fa fa-undo" aria-hidden="true"></i>';
-
-const tr = btn.parentElement.parentElement.parentElement;
-const inputs = tr.querySelectorAll('input');
-console.log(inputs)
-inputs.forEach(input => {
-  if (input.name !== 'id'){
-    input.removeAttribute('readonly')
-  }
-})
-btn.onclick = () => {if (validateUser(tr)){
-  showMessage('Changes saved.', 'success', tr);
-  setTimeout(()=>saveUser(tr),5000)
-  inputs.forEach(input => input.setAttribute('readonly','true'))
-}}
-btnUndo.onclick = () => getUsers('http://localhost:3000/users')
-   document.querySelectorAll('button').forEach(button => {
-     if (!tr.contains(button)){
-       button.onclick = () => {
-         showMessage("You can't edit multiple users at once.", 'danger', tr)
-       }
-     }
-   })
-}
-
-const validateUser = tr => {
-  const data = getRowData(tr);
-  const patterns = {
-    id: /.*/,
-    name: /^[A-Z][a-z]* [A-Z][a-z]*$/,
-    address: /^\d* ([A-Z0-9][a-z0-9 ]*)+$/,
-    email:    /^[a-z0-9\-.]+@[a-z0-9\-.]+\.[a-z]{2,4}$/
-  }
-  if (Object.keys(data).some(key => data[key].match(patterns[key]) === null)){
-    showMessage('Invalid input!', 'danger', tr)
-    return false
-  } else return true
-}
-
-const saveUser = (tr) => {
-  const data = getRowData(tr);
-  
-  nonWriteableUsers.push(data.id)
-const fetchOptions = {
-  method: "PUT",
-  mode: "cors",
-  cache: "no-cache",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify(data)
-}
-fetch(`http://localhost:3000/users/${data.id}`, fetchOptions).then(
-  resp => resp.json(),
-  err => console.error(err)
-).then(
-  () => getUsers('http://localhost:3000/users')
-  )
-}
-
-const showMessage = (message, type, row) => {
-  const td = row.insertCell();
-  td.classList.add(type);
-  td.textContent = message;
-  setTimeout(() =>{
-    row.removeChild(td)
-  }, 5000)
+export {
+  createAnyElement,
+  userKeys,
+  getUsers,
+  nonWriteableUsers
 }
